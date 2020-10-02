d5656aea01e835706449267016d7fa4e48246a43
Arthur Freitas <bfreitas.arthur@gmail.com>
2020-10-02 11:10:53 -0300

    Adiciona teste que cobre o caso de envio de reports por email

--- lib/ims_report/service/mail_service.ex
+++ lib/ims_report/service/mail_service.ex
@@ -1,7 +1,7 @@
 defmodule ImsReport.Service.MailService do
   use Tesla
 
-  plug Tesla.Middleware.BaseUrl, Application.get_env(:ims, :mail_service_api_url)
+  plug Tesla.Middleware.BaseUrl, Application.get_env(:ims_report, :mail_service_api_url)
   plug Tesla.Middleware.JSON
 
   def sendReport(report, email) do
