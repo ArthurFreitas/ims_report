e84b538555eb0185175a149045f743eccda6d63e
Arthur Freitas <bfreitas.arthur@gmail.com>
2020-10-06 20:58:50 -0300

    Adiciona configuração básica do sentry

--- lib/ims_report_web/controllers/page_controller.ex
+++ lib/ims_report_web/controllers/page_controller.ex
@@ -4,4 +4,9 @@ defmodule ImsReportWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+
+  def sentry_check(_conn, _params) do
+    raise "Hello from report service"
+  end
 end
